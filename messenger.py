e5a613887409b4386531420abae6b3b7e0409b39
Kelvin Rodriguez <kelvinrr@icloud.com>
2017-05-29 20:23:33 -0700

    changed token

--- messenger.py
+++ messenger.py
@@ -28,7 +28,7 @@ WIT_TOKEN = 'FCLEILUP6T2PTIH6TSWWJYFJYKG3KL2L'
 # Messenger API parameters
 FB_PAGE_TOKEN = 'EAAEvbHrTo9IBAJigS9lKANutM4V3qOzcnzi86PbWufrN5NJaB1c8ZBOn1DEof3lrTPX9w3qZCpWn93G9yRLw5UrtzS4dP6HLmuAenhAjKJUXDMVP67Iq2FHr2Fc3yCyZBgF87ZAj0y1PPshW0elBivNr0vXw6UPxY5BsyZA1u3gZDZD'
 # A user secret to verify webhook get request.
-FB_VERIFY_TOKEN = '4790ec57e6aecac2fb5de2d039dcc708'
+FB_VERIFY_TOKEN = 'hello'
 
 # Setup Bottle Server
 debug(True)
