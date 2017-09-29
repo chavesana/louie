ee7a6ea9acaca002c3abfc8b512decb5c1265dfa
Tyler Thatcher <thatcher@Tylers-MacBook-Pro.local>
2017-09-28 20:22:07 -0700

    Added a functional query function

--- louie/query.py
+++ louie/query.py
@@ -1,7 +1,21 @@
 from louie import wolfclient
 
-def wolfram_query(string):
-    """
-    """
-    query_result = wolfclient.query(string)
-    return next(query_result.results).text
+def wolfram_search(simple_question):
+    print('performing wolfram search-------------')
+    print(question)
+    query_result = wolfclient.query(str(simple_question))
+    i = 0
+
+    if(query_result['@success'] == 'false'):
+        print('We did not find an answer for your question.')
+
+    elif(query_result.results):
+        print('I did not find a straight forward answers, here is what I found: \n')
+        for pod in query_result.pods:
+            for sub in pod.subpods:
+                i += 1
+                if(i == 6):
+                    print(sub['img']['@title'], i)
+                    break
+    else:
+        print(next(query_result.results).text)
