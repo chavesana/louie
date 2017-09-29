7d1df35812a7fd7e199545ccccb71fc40c421ebb
Tyler Thatcher <thatcher@Tylers-MacBook-Pro.local>
2017-09-28 21:56:13 -0700

    Edited query function

--- louie/query.py
+++ louie/query.py
@@ -3,17 +3,17 @@ from louie import wolfclient
 def wolfram_search(simple_question):
     print('performing wolfram search-------------')
     query_result = wolfclient.query(str(simple_question))
-    i = 0
+    sub_pod_num = 0
 
     if(query_result['@success'] == 'false'):
         return 'We did not find an answer for your question.'
 
-    elif(query_result.results):
-        print('I did not find a straight forward answers, here is what I found: \n')
+    elif(list(query_result.results)):
+         return next(query_result.results).text
+
+    else:
         for pod in query_result.pods:
             for sub in pod.subpods:
-                i += 1
-                if(i == 6):
-                    return 'I did not find a straight forward answers, here is what I found: \n' + sub['img']['@title']
-    else:
-        return next(query_result.results).text
+                sub_pod_num += 1
+                if(sub_pod_num == 6):
+                    return sub['img']['@title']
