ce900b8173e408cf2b9c50d38c773a8dabf31c75
Kelvin Rodriguez <kelvinrr@icloud.com>
2017-10-07 21:32:58 -0700

    Added readme and setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup, find_packages
+import louie
+
+#Grab the README.md for the long description
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+VERSION = louie.__version__
+
+def setup_package():
+    setup(
+        name = "louie",
+        version = VERSION,
+        author = "Kelvin Rodriguez, Tyler Thatcher",
+        author_email = "kr788@nau.edu",
+        description = ("Chatbot supporting general use queries"),
+        long_description = long_description,
+        license = "Public Domain",
+        keywords = "chat bot smart city",
+        url = "http://packages.python.org/louie",
+        packages=find_packages(),
+        include_package_data=True,
+        package_data=None,
+        zip_safe=False,
+        install_requires=[
+            'pandas',
+            'psycopg2',
+            'pymongo',
+            'requests',
+            'bottle',
+            'wit',
+            'python-google-places',
+            'wolframalpha',
+            'fuzzywuzzy'
+            ],
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Topic :: Utilities",
+            "License :: Public Domain",
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+        ],
+    )
+
+if __name__ == '__main__':
+    setup_package()
